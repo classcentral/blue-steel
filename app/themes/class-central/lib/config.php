56de00d964a4a58caf8dfe315fcc05285044ab51
indrek-k <indrek@codelight.eu>
2014-05-28 15:46:57 +0000

    Restructured base

--- app/themes/class-central/lib/config.php
+++ app/themes/class-central/lib/config.php
@@ -55,14 +55,12 @@ function roots_display_sidebar() {
      */
     array(
       'is_404',
-      'is_front_page'
     ),
     /**
      * Page template checks (via is_page_template())
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
     )
   );
 
---
+++ app/themes/class-central/templates/index/index-content.php
@@ -0,0 +1,31 @@
+<main class="main <?php echo roots_main_class(); ?>" role="main">
+
+  <?php if (!have_posts()) : ?>
+    <div class="alert alert-warning">
+      <?php _e('Sorry, no results were found.', 'roots'); ?>
+    </div>
+    <?php get_search_form(); ?>
+  <?php endif; ?>
+
+  <?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class(); ?>>
+      <header>
+        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        <?php get_template_part('templates/entry-meta'); ?>
+      </header>
+      <div class="entry-summary">
+        <?php the_excerpt(); ?>
+      </div>
+    </article>
+  <?php endwhile; ?>
+
+  <?php if ($wp_query->max_num_pages > 1) : ?>
+    <nav class="post-nav">
+      <ul class="pager">
+        <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+        <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+      </ul>
+    </nav>
+  <?php endif; ?>
+
+</main><!-- /.main -->
