e038edf2f15e544ea054926ee39cbdecd87be4c6
indrek-k <indrek@codelight.eu>
2014-05-29 09:45:57 +0000

    Blog index templates, backend

--- app/themes/class-central/index.php
+++ app/themes/class-central/index.php
@@ -1,3 +1,21 @@
+<?php
+
+$args = array(
+  'posts_per_page' => 1,
+  'paged' => 0,
+  'meta_query' => array(
+      'key' => 'pinned',
+      'value' => 'true'
+  ),
+  'post_type' => 'post'
+);
+
+$pinned = get_posts($args);
+
+$banner = get_field('banner', $pinned[0]->ID);
+//var_dump($banner);
+
+?>
 
 <?php include(locate_template('templates/index/index-banner.php')); ?>
 
--- app/themes/class-central/lib/fields.php
+++ app/themes/class-central/lib/fields.php
@@ -38,16 +38,27 @@ if(function_exists("register_field_group"))
     ),
     'menu_order' => 0,
   ));
+
   register_field_group(array (
-    'id' => 'acf_post-thumbnail',
-    'title' => 'Post Thumbnail',
+    'id' => 'acf_post-images',
+    'title' => 'Post Images',
     'fields' => array (
+      array (
+        'key' => 'field_5386f660e3121',
+        'label' => 'Banner',
+        'name' => 'banner',
+        'type' => 'image',
+        'instructions' => '1600x600 image used in the header of the blog, archive or post page. If empty, a single color background will be used instead.',
+        'save_format' => 'object',
+        'preview_size' => 'thumbnail',
+        'library' => 'all',
+      ),
       array (
         'key' => 'field_5386f410cb892',
         'label' => 'Thumbnail',
         'name' => 'thumbnail',
         'type' => 'image',
-        'instructions' => 'Thumbnail used on Blog and Archive pages. If empty, the featured image will be used instead.',
+        'instructions' => '400x265 thumbnail used on Blog and Archive pages. If empty, the featured image will be used instead.',
         'save_format' => 'object',
         'preview_size' => 'thumbnail',
         'library' => 'all',
@@ -71,5 +82,40 @@ if(function_exists("register_field_group"))
       ),
     ),
     'menu_order' => 0,
+  ));
+  register_field_group(array (
+    'id' => 'acf_pinned',
+    'title' => 'Pinned',
+    'fields' => array (
+      array (
+        'key' => 'field_5386f89ac67db',
+        'label' => 'Pin this post',
+        'name' => 'pinned',
+        'type' => 'checkbox',
+        'choices' => array (
+          'true' => 'Pinned',
+        ),
+        'default_value' => '',
+        'layout' => 'vertical',
+      ),
+    ),
+    'location' => array (
+      array (
+        array (
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'post',
+          'order_no' => 0,
+          'group_no' => 0,
+        ),
+      ),
+    ),
+    'options' => array (
+      'position' => 'normal',
+      'layout' => 'default',
+      'hide_on_screen' => array (
+      ),
+    ),
+    'menu_order' => -1,
   ));
 }
