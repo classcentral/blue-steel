45db41436a1dcfbedccc76a57088aec198445d88
matsjoonas <mj@codelight.eu>
2014-05-29 15:25:15 +0300

    index content and banner styles

--- app/themes/class-central/index.php
+++ app/themes/class-central/index.php
@@ -20,6 +20,8 @@ $banner = get_field('banner', $pinned[0]->ID);
 <?php include(locate_template('templates/index/index-banner.php')); ?>
 
 <div class="container">
-  <?php include(locate_template('templates/index/index-content.php')); ?>
-  <?php include roots_sidebar_path(); ?>
+    <div class="row">
+        <?php include(locate_template('templates/index/index-content.php')); ?>
+        <?php include roots_sidebar_path(); ?>
+    </div>  
 </div>
