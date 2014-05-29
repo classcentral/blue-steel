9c15b9a3d3465930383e98c3a7745b2ee8ae1271
indrek-k <indrek@codelight.eu>
2014-05-29 10:56:16 +0000

    Single article

--- app/themes/class-central/templates/single/single-banner.php
+++ app/themes/class-central/templates/single/single-banner.php
@@ -1,9 +1,12 @@
-
-<section class="header-area" style="background: url() center top no-repeat">
+<section class="header-area" style="background-image: url('<?php echo $banner['sizes']['blog-banner-full']; ?>');">
     <div class="container">
         <div class="header-content">
             <h1 class="free-online-ed"><?php echo roots_title(); ?></h1>
-            <h2>A subline something something something something something</h2>
+            <?php if ($subtitle = get_field('subtitle')): ?>
+              <h4>
+                <?php echo $subtitle; ?>
+              </h4>
+            <?php endif; ?>
         </div>
     </div><!-- /container -->
 </section>
