cd23671637f373742f01ea13aa53a02828b0c7ad
matsjoonas <mj@codelight.eu>
2014-05-29 12:47:17 +0300

    setup

--- app/themes/class-central/templates/single/single-banner.php
+++ app/themes/class-central/templates/single/single-banner.php
@@ -1,5 +1,9 @@
-<div class="page-header">
-  <h1>
-    <?php echo roots_title(); ?>
-  </h1>
-</div>
+
+<section class="header-area" style="background: url() center top no-repeat">
+    <div class="container">
+        <div class="header-content">
+            <h1 class="free-online-ed"><?php echo roots_title(); ?></h1>
+            <h2>A subline something something something something something</h2>
+        </div>
+    </div><!-- /container -->
+</section>
