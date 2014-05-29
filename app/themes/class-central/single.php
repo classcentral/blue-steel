5f7d5d86524230d0a28c4f3f48a5863e7e5de919
indrek-k <indrek@codelight.eu>
2014-05-29 10:56:16 +0000

    Single article

--- app/themes/class-central/single.php
+++ app/themes/class-central/single.php
@@ -2,6 +2,8 @@
 <?php include(locate_template('templates/single/single-banner.php')); ?>
 
 <div class="container">
-  <?php include(locate_template('templates/single/single-content.php')); ?>
-  <?php include roots_sidebar_path(); ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <?php include(locate_template('templates/single/single-content.php')); ?>
+    <?php include roots_sidebar_path(); ?>
+  <?php endwhile; ?>
 </div>
