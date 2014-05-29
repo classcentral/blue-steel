7c38d92636a0a19bf5c5271891ef0bc5d8420baa
matsjoonas <mj@codelight.eu>
2014-05-29 18:07:04 +0300

    header and footer

--- app/themes/class-central/templates/layout/header.php
+++ app/themes/class-central/templates/layout/header.php
@@ -1,14 +1,104 @@
-<header class="banner container" role="banner">
-  <div class="row">
-    <div class="col-lg-12">
-      <a class="brand" href="<?php echo home_url('/') ?>"><?php bloginfo('name'); ?></a>
-      <nav class="nav-main" role="navigation">
-        <?php
-          if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills'));
-          endif;
-        ?>
-      </nav>
-    </div>
-  </div>
-</header>
+<nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/" title="MOOC aggregator">
+                <span class="cc-logo-navbar"></span>
+                <svg role="logo" aria-label="class central logo" viewBox="0 0 332 32" class="class-central-logo">
+                    <use xlink:href="#icon-cc-logo"></use>
+                </svg>
+                Class Central
+            </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle nav-title" title="Browse MOOCs by their Start date" data-toggle="dropdown">
+                        Courses
+                        <b class="caret">
+                            <svg class="icon icon-cc-logo7" viewBox="0 0 32 32"><use xlink:href="#icon-cc-logo7"></use></svg>
+                        </b>
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a href="/courses/recent" title="Recently started or starting soon - MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Courses', 'recent'])">Recently started or starting soon (42)</a></li>
+                        <li><a href="/courses/ongoing" title="Courses in Progress - MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Courses', 'ongoing'])">Courses in Progress (201)</a></li>
+                        <li><a href="/courses/upcoming" title="Future courses - MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Courses', 'upcoming'])">Future courses (224)</a></li>
+                        <li><a href="/courses/selfpaced" title="Self Paced - MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Courses', 'selfpaced'])">Self Paced (71)</a></li>
+                        <li><a href="/courses/past" title="Finished courses - MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Courses', 'past'])">Finished courses (923)</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle nav-title" title="Browser MOOCs by Initiatives" data-toggle="dropdown">
+                        Providers
+                        <b class="caret">
+                            <svg class="icon icon-cc-logo7" viewBox="0 0 32 32"><use xlink:href="#icon-cc-logo7"></use></svg>
+                        </b>
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a href="/provider/coursera" title="List of Coursera MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'coursera'])">Coursera (644)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/udacity" title="List of Udacity MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'udacity'])">Udacity (33)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/edx" title="List of EdX MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'edx'])">EdX (131)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/novoed" title="List of NovoED MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'novoed'])">NovoED (25)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/canvas" title="List of Canvas.net MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'canvas'])">Canvas.net (104)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/open2study" title="List of Open2Study MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'open2study'])">Open2Study (50)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/iversity" title="List of iversity MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'iversity'])">iversity (28)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/futurelearn" title="List of FutureLearn MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'futurelearn'])">FutureLearn (44)</a>
+                      </li>
+                      <li>
+                        <a href="/provider/others" title="List of Others MOOCs" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Initiative', 'others'])">Others (389)</a>
+                      </li>
+                    </ul>
+                </li>
+
+                                <li><a href="/subjects" class="nav-title" title="Subjects" onclick="_gaq.push(['_trackEvent', 'navbar-clicks', 'Subjects'])">Subjects</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="social-icon">
+                    <a href="http://www.facebook.com/classcentral" target="_blank" onmousedown="_gaq.push(['_trackEvent', 'navbar-clicks', 'Social Media','Facebook'])">
+                        Facebook
+                        <span class="fb-icon-png"></span>
+                        <svg class="icon icon-facebook" viewBox="0 0 32 32"><use xlink:href="#icon-facebook"></use></svg>
+                    </a>
+                </li>
+                <li class="social-icon">
+                    <a href="https://twitter.com/classcentral" target="_blank" onmousedown="_gaq.push(['_trackEvent', 'navbar-clicks', 'Social Media','Twitter'])">
+                        Twitter
+                        <span class="twitter-icon-png"></span>
+                        <svg class="icon icon-twitter" viewBox="0 0 32 32"><use xlink:href="#icon-twitter"></use></svg>
+                    </a>
+                </li>
+                <li class="social-icon">
+                    <a href="https://plus.google.com/+ClassCentral" target="_blank" onmousedown="_gaq.push(['_trackEvent', 'navbar-clicks', 'Social Media','Google+'])">
+                        Google plus
+                        <span class="gplus-icon-png"></span>
+                        <svg class="icon icon-google_follow" viewBox="0 0 32 32"><use xlink:href="#icon-google_follow"></use></svg>
+                    </a>
+                </li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container -->
+</nav>
