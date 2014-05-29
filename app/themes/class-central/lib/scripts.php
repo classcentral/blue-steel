53b14a449851aeeac4870238e6ae2e27a5948c53
matsjoonas <mj@codelight.eu>
2014-05-29 16:39:40 +0300

    index styles

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e2e9ccb68120f157d260738b59d9889c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '224f1b093de83dc2f64745b6b355c4a1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
