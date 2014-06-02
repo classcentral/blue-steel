4cba0b2422521f93b0e9f490c9070aa3e36eed7d
Indrek <indrek@indrek.it>
2014-06-02 23:59:18 +0300

    Added post and author meta

--- app/themes/class-central/templates/single/single-banner.php
+++ app/themes/class-central/templates/single/single-banner.php
@@ -11,9 +11,9 @@
               <span class="read-icon">
                 <svg class="icon icon-read-icon" viewBox="0 0 26 32"><use xlink:href="#icon-read-icon"></use></svg>
               </span>
-              <span class="read-length">7 minute read</span>
-              <span class="written-by">&nbsp;written by</span>
-              <a href="#" class="cc-post-author">&nbsp;Benedict Cumberbatch</a>
+              <span class="read-length"><?php echo cc_get_reading_time(); ?> read&nbsp;</span>
+              <span class="written-by">written by&nbsp;</span>
+              <a href="#" class="cc-post-author"><?php echo cc_get_author_name(); ?></a>
             </div>
         </div>
     </div><!-- /container -->
