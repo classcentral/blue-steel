c2505049905a1ebb99df6b8bf71a8ca3bf2b8195
matsjoonas <mj@codelight.eu>
2014-06-03 12:52:38 +0300

    responsive tweaks

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c785ffcf69b58e77658cb3240d081307');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd55303e1fd3b3ae7d48b72e83b489dd5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
