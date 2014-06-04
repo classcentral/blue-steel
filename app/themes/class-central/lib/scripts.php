db730a200d2df0878d5cb5bae9daee27ad8a4190
Indrek <indrek@indrek.it>
2014-06-04 12:03:23 +0300

    Modifications to blockquote styling

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd29e0cd572ea5b9718dbbe1fef01fc52');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5ebc3550ac16013f46551c68b93157c6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
