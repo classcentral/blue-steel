bec5c53a0de681aeb540db01c56cbadfcc88cdd7
matsjoonas <mj@codelight.eu>
2014-06-04 13:14:56 +0300

    blockquote width

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '705e502f69bdec86c4e6fbed6d19451f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a83e2161b3f79aaf3a4af7e6abaf07c8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
