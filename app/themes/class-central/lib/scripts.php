145c6fec38349ba623be27155a33703bd53062a9
matsjoonas <mj@codelight.eu>
2014-06-06 18:11:57 +0300

    vide desecription styles

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a3b642ec40d8982ca95c1bee56707b1a');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7ad3443bc59c6acc052f8062d979039b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
