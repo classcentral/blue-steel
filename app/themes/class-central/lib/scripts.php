a9601a02ed020dbd89d9c045cf83762fb55b8fea
matsjoonas <mj@codelight.eu>
2014-06-09 11:45:29 +0300

    blockquote tweaks

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b33b49b4fe0c7ab2c8fe298320e3fed5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7cc85bb6dc326203eada1692fb126385');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
