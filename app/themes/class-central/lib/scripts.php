3822260af15e6b8ee68a21371f4c9f36766c72c6
matsjoonas <mj@codelight.eu>
2014-06-09 11:42:47 +0300

    blockquote width tweaks

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3d91a675f3085fef0bc491e8c4cc7a9c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b33b49b4fe0c7ab2c8fe298320e3fed5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
