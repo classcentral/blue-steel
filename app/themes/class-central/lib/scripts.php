be092ed218d8bc6ba85d6dc2326f18388eba40f5
matsjoonas <mj@codelight.eu>
2014-07-30 16:04:15 +0300

    sidebar styles and dummy html

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '071b7bf9d704f47b1ba444423846cb3b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4b1c460375437b43c60f524c62cc1a6b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
@@ -27,7 +27,7 @@ function roots_scripts() {
   }
 
   wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.7.0.min.js', array(), null, false);
-  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'ee6ccb5f9058e0c7ecb92b8da74ab383', true);
+  wp_register_script('roots_scripts', get_template_directory_uri() . '/assets/js/scripts.min.js', array(), 'e5031990e8f9435f05512288f1847e08', true);
   wp_enqueue_script('modernizr');
   wp_enqueue_script('jquery');
   wp_enqueue_script('roots_scripts');
--- app/themes/class-central/templates/sidebar.php
+++ app/themes/class-central/templates/sidebar.php
@@ -1,6 +1,82 @@
-<?php /*
+
 <?php if (roots_display_sidebar()) : ?>
-  <aside class="sidebar col-sm-4">
+  <aside class="sidebar col-sm-3">
+    <?php if (is_home()) : ?>
+
+    <?php else : ?>
+      <?php //dynamic_sidebar('sidebar-primary'); ?>
+
+      <section class="widget">
+        <h3 class="section-heading">
+            <span>Featured Post</span>
+        </h3>
+        <ul>
+          <li>
+            <div class="crop-wrap"><img src="http://placehold.it/85x58" alt="post thumbnail"></div>
+            <div class="post-info-wrap">
+              <a href="/futurelearn-350k-learners/">Futurelearn gains steam with 350,000 learners and 7 new partner institutions</a>
+              <span class="post-date">June 10, 2014</span>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <section class="widget recent-posts-3 widget_recent_entries">
+        <h3 class="section-heading">
+            <span>Latest Posts</span>
+        </h3>
+        <ul>
+          <li>
+            <div class="crop-wrap"><img src="http://placehold.it/85x58" alt="post thumbnail"></div>
+            <div class="post-info-wrap">
+              <a href="/futurelearn-350k-learners/">Futurelearn gains steam with 350,000 learners and 7 new partner institutions</a>
+              <span class="post-date">June 10, 2014</span>
+            </div>
+          </li>
+          <li>
+            <div class="crop-wrap"><img src="http://placehold.it/85x58" alt="post thumbnail"></div>
+            <div class="post-info-wrap">
+              <a href="/japanese-moocs-from-gacco-and-jmooc/">The first Japanese MOOCs from gacco and JMOOC</a>
+              <span class="post-date">June 2, 2014</span>
+            </div>
+          </li>
+          <li>
+            <div class="crop-wrap"><img src="http://placehold.it/85x58" alt="post thumbnail"></div>
+            <div class="post-info-wrap">
+              <a href="/study-crime-for-credit-penn-state-mooc/">Study Crime for Credit with a Penn State MOOC</a>
+              <span class="post-date">May 30, 2014</span>
+            </div>
+          </li>
+          <li>
+            <div class="crop-wrap"><img src="http://placehold.it/85x58" alt="post thumbnail"></div>
+            <div class="post-info-wrap">
+              <a href="/downton-abbey-futurelearn-mooc/">Downton Abbey, Americans, and an English Country House MOOC</a>
+              <span class="post-date">May 26, 2014</span>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <section class="widget categories-3 widget_categories">
+        <h3 class="section-heading">
+            <span>Categories</span>
+        </h3>
+        <ul>
+          <li class="cat-item cat-item-61"><a href="/category/interviews/" title="View all posts filed under Interviews">Interviews</a>
+        </li>
+          <li class="cat-item cat-item-26"><a href="/category/mooc-commentary/" title="View all posts filed under MOOC Commentary">MOOC Commentary</a>
+        </li>
+          <li class="cat-item cat-item-8"><a href="/category/mooc-interview-2/" title="View all posts filed under MOOC Interview">MOOC Interview</a>
+        </li>
+          <li class="cat-item cat-item-3"><a href="/category/mooc-provider-walkthrough/" title="View all posts filed under MOOC Provider Walkthrough">MOOC Provider Walkthrough</a>
+        </li>
+          <li class="cat-item cat-item-1"><a href="/category/uncategorized/" title="View all posts filed under Uncategorized">Uncategorized</a>
+        </li>
+            </ul>
+      </section>
+
+    <?php endif; ?>
+    <!--
     <div class="ad-wrap">
       <div class="ad-content">
 
@@ -28,6 +104,8 @@
       </div>
       <span class="ad-note">Advertisement</span>
     </div>
+    -->
+
   </aside><!-- /.sidebar -->
 <?php endif; ?>
- */ ?>
+
