e98425588ba7f97aca76b1d636607adbe9a98511
matsjoonas <mj@codelight.eu>
2014-12-07 15:58:25 +0200

    star ratings and widget top bar

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3e4c90a856e17c693349e244667f9dec');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e312d9aff7772d580baed6ed2458675c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- app/themes/class-central/templates/single/single-content.php
+++ app/themes/class-central/templates/single/single-content.php
@@ -14,6 +14,180 @@
           <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="cc-post-author"><?php echo cc_get_author_name(); ?></a>
         </div>
         <?php the_content(); ?>
+
+
+        <div class="wideBlock">
+          <div class="reviewWidget">
+
+            <div class="reviewWidget_topBar">
+              <svg role="logo" aria-label="class central logo" viewBox="0 0 332 32" class="class-central-logo">
+                  <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-cc-logo"></use>
+              </svg>
+              <span class="reviewWidget_topBar_title">Review</span>
+              <a href="https://www.class-central.com" class="reviewWidget_topBar_link">class-central.com</a>
+            </div>
+
+            <div class="reviewWidget_header">
+              <h2 class="reviewWidget_title"></h2>
+            </div>
+
+            <div class="reviewWidget_body">
+
+            </div>
+
+            <div class="reviewWidget_footer">
+              <div class="starRating rating45">
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+              </div>
+
+              <div class="starRating rating30 starRating-small starRating-gray">
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+                <div class="starRating_star">
+                  <svg class="starRating_starShape starRating_starShape-blank">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-full">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-full"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-left starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-left"></use>
+                  </svg>
+                  <svg class="starRating_starShape starRating_starShape-right starRating_starShape-half">
+                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-star-half-right"></use>
+                  </svg>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+
       </div>
       <footer>
         <?php get_template_part('templates/single/single-footer'); ?>
