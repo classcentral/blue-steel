b4fade7c10c439c97c248508258e1cff04342a9e
matsjoonas <mj@codelight.eu>
2014-12-07 18:18:22 +0200

    review widget

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e312d9aff7772d580baed6ed2458675c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '59fe3fdecb454efc5e3deb52930189c4');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
