c29cf7f9d2c262e9e048b465e2eb38e7dd6995a4
Dhawal Shah <dhawal@class-central.com>
2015-06-01 22:40:17 -0700

    Add new newsletter popup form

--- app/themes/class-central/lib/scripts.php
+++ app/themes/class-central/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4d9e94765803d2ba36cf0ea3417808e7');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f19f752c28648b71340021e41f21b07e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
